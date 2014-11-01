52cec8a74c68014a973991244dcb89ec005a79a6
ruimangas <ruijosemangas@gmail.com>
2014-11-01 16:13:43 +0000

    minor changes

--- my_project/src/main/java/pt/ist/p2p/masterPeer.java
+++ my_project/src/main/java/pt/ist/p2p/masterPeer.java
@@ -1,11 +1,7 @@
 package main.java.pt.ist.p2p;
 
-import java.io.IOException;
-import java.util.*;
-
 import net.tomp2p.connection.Bindings;
 import net.tomp2p.futures.FutureDHT;
-import net.tomp2p.futures.FutureBootstrap;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerMaker;
 import net.tomp2p.peers.Number160;
@@ -13,43 +9,41 @@ import net.tomp2p.storage.Data;
 
 
 
-public class masterPeer {
+public class MasterPeer {
 
-    
-     private static Peer peerMaster;
-     
-        
-    public masterPeer(){
-              
-    }
-    
+    private static Peer peerMaster;
 
-  public static void main(String[] args ){
-      
-      Random rnd = new Random();
-      Bindings b = new Bindings();
-      Number160 id = new Number160(1);
-      
-      
-      try{
-      
-          peerMaster = new PeerMaker(id).setTcpPort(10001).setUdpPort(10001).setBindings(b).makeAndListen();
-          peerMaster.getConfiguration().setBehindFirewall(true);
-          peerMaster.put(Number160.createHash("master")).setData(new Data("teste")).start().awaitUninterruptibly();
-      }catch(Exception e){
-          
-          System.out.println(e.getMessage());
-      }
-      
-      try {
-        Thread.sleep(50000);
-    } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+    public MasterPeer(){
+
     }
-     
-      System.out.println("my peers:" + peerMaster.getPeerBean().getPeerMap().getAll());
-  }
+
+
+    public static void main(String[] args ){
+
+        Bindings b = new Bindings();
+        Number160 id = new Number160(1);
+
+
+        try{
+
+            peerMaster = new PeerMaker(id).setTcpPort(10001).setUdpPort(10001).setBindings(b).makeAndListen();
+            peerMaster.getConfiguration().setBehindFirewall(true);
+            peerMaster.put(Number160.createHash("master")).setData(new Data("teste")).start().awaitUninterruptibly();
+
+
+        }catch(Exception e){
+
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Thread.sleep(50000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("my peers:" + peerMaster.getPeerBean().getPeerMap().getAll());
+    }
     
  }
     
--- my_project/src/main/java/pt/ist/p2p/tomp2p.java
+++ my_project/src/main/java/pt/ist/p2p/tomp2p.java
@@ -16,26 +16,19 @@ import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.storage.Data;
 
-
-
-
-public class tomp2p {
+public class Tomp2p {
     
     private static Peer peer1 = null;
     private static User u = new User();
-        
-    public tomp2p()  {
-        
-        
+
+    public Tomp2p()  {
+
     }        
-   
-  //  public static Peer MasterPeer = masterPeer.getMasterPeer();
-    
+       
     public static void PeerBuilder() throws ClassNotFoundException, IOException{
         
         Random rnd = new Random();
         Bindings b = new Bindings();
-     // b.addInterface("eth0");
         
         peer1 = new PeerMaker(new Number160(rnd)).setTcpPort(10002).setUdpPort(10002).setBindings(b).makeAndListen();
         
@@ -44,28 +37,25 @@ public class tomp2p {
         InetAddress address = Inet4Address.getByName("194.210.234.232");
        
         PeerAddress peerAddress = new PeerAddress(new Number160(1),address,10001,10001);
-        
-        
+
         FutureDiscover future = peer1.discover().setPeerAddress(peerAddress).start();
         future.awaitUninterruptibly();
         System.out.println("depois do discover");
         FutureBootstrap future1 = peer1.bootstrap().setPeerAddress(peerAddress).start();
         future1.awaitUninterruptibly();
-        System.out.println("my peers:" + peer1.getPeerBean().getPeerMap().getAll()); 
-        
+        System.out.println("my peers:" + peer1.getPeerBean().getPeerMap().getAll());
+
         
         FutureDHT futureDHT = peer1.get(Number160.createHash("master")).start();
         futureDHT.awaitUninterruptibly();
         if (futureDHT.isSuccess()) {
             System.out.println(futureDHT.getData().getObject().toString());
         }
-        
-       }
+
+    }
 
     public static void comandLine(){
-                    
-        String i = "";
-        String[] commands;
+
         Scanner keyboard = new Scanner(System.in);
 
         try{
@@ -102,16 +92,12 @@ public class tomp2p {
     }
 
     public static void offerItem(){
-        int j = 0;
+
         System.out.println("Please, enter the name of the product:");
         Scanner keyboard = new Scanner(System.in);
-        String item = keyboard.nextLine();
-        u.setOfferedItem(item);
+        String itemTitle = keyboard.nextLine();
+        u.setOfferedItem(itemTitle);
 
-        for(String i : u.getOfferedItems()){   
-            System.out.println("Item " + j + " : " + i);
-            j+=1;
-        }
     }
 
     public static void acceptBid(){
@@ -129,7 +115,7 @@ public class tomp2p {
         try{ 
 
             BufferedReader br = new BufferedReader(new FileReader("users.txt"));  
-            String line = null;
+            String line;
 
             while ((line = br.readLine()) != null){
                 if(user.equals(line)){
@@ -147,10 +133,9 @@ public class tomp2p {
 
     public static boolean passVerifier(){
 
-        boolean accepted = false;
+        boolean accepted;
         Scanner sc = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
-
+      
         while(true){
 
             System.out.println("User:");
@@ -172,14 +157,14 @@ public class tomp2p {
     public static void main (String[] args){
         
         
-       if(tomp2p.passVerifier()){
+       if(Tomp2p.passVerifier()){
            try{
                System.out.println("antes do PeerBuilder");
-            tomp2p.PeerBuilder();
+            Tomp2p.PeerBuilder();
            }catch(Exception e){
                System.out.println(e.getMessage());
            }
-        tomp2p.comandLine();
+        Tomp2p.comandLine();
        }
         
     }
