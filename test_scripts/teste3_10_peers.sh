7c130e1d0820703875bfe365c7bbca6c7c916ec2
João Domingos <jota_domingos@hotmail.com>
2014-12-05 17:13:34 +0000

    tests done and working

---
+++ test_scripts/teste3_10_peers.sh
@@ -0,0 +1,10 @@
+ssh -f istple_seprs4@onelab3.warsaw.rd.tp.pl 'cd my_project; ant -Darg0=3000 runTestAcceptBid'
+ssh -f istple_seprs4@ple2.ipv6.lip6.fr 'cd my_project; ant -Darg0=3001 runIdle'
+ssh -f istple_seprs4@ple1.dmcs.p.lodz.pl 'cd my_project; ant -Darg0=3002 runIdle'
+ssh -f istple_seprs4@planetlab1.cs.uoi.gr 'cd my_project; ant -Darg0=3003 runIdle'
+ssh -f istple_seprs4@iraplab2.iralab.uni-karlsruhe.de 'cd my_project; ant -Darg0=3004 runIdle'
+ssh -f istple_seprs4@planetlab1.di.unito.it 'cd my_project; ant -Darg0=3005 runIdle'
+ssh -f istple_seprs4@mars.planetlab.haw-hamburg.de 'cd my_project; ant -Darg0=3006 runIdle'
+ssh -f istple_seprs4@planetlab2.jcp-consult.net 'cd my_project; ant -Darg0=3007 runIdle'
+sleep 6s
+ssh istple_seprs4@prata.mimuw.edu.pl  'cd my_project; ant -Darg0=3008 runTestSearchItemRemoved'
